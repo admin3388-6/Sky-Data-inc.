af6888070ba3d6166d983f3532f3e5080195381d
admin3388-6 <lolipop1999x@gmail.com>
2025-11-26 15:05:41 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://xjqymtaqletnbyhjdffp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcXltdGFxbGV0bmJ5aGpkZmZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMTU1ODMsImV4cCI6MjA3OTU5MTU4M30.2wtdWOqJUtt2ZukA-PsDclVUr0P3Pzii285k-rIGW-0"
+);
